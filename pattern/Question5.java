cb21808e7bfba692c54e9032a0944be1cc31b01c
Vishwas Kumar <vishwassingh118866@gmail.com>
2024-01-29 17:38:54 +0530

    optimise

--- pattern/Question5.java
+++ pattern/Question5.java
@@ -3,23 +3,17 @@ package DSA_with_JAVA.pattern;
 public class Question5 {
     public static void pattern(int n)
     {
-        for(int row=1;row<=n;row++)
+        for(int row=1;row<=2*n;row++)
         {
-            for(int col=1;col<=row;col++)
+            int formula=row>n?2*n-row-1:row;
+            for(int col=1;col<=formula;col++)
             {
                 System.out.print("* ");
             }
             System.out.println();
         }
 
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=n-i+1;j++)
-            {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+
     }
     public static void main(String[] args) {
         pattern(5);
