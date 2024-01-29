f54dda587dbf74811d5cf290915b8d69ce8ede6e
Vishwas Kumar <vishwassingh118866@gmail.com>
2024-01-29 11:03:26 +0530

    sixth commit

---
+++ pattern/Question6.java
@@ -0,0 +1,29 @@
+package DSA_with_JAVA.pattern;
+
+public class Question6 {
+    public static void pattern(int n)
+    {
+        for(int row=1;row<=n;row++)
+        {
+            for(int col=1;col<=7;col++)
+            {
+                if(col>=5-row && col<=3+row)
+                {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+
+            }
+            System.out.println();
+        }
+
+
+    }
+    public static void main(String[] args) {
+        pattern(4);
+    }
+
+}
