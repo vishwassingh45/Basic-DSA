98b52cc34759604ffce484f3888af5c0c471ac0d
Vishwas Kumar <vishwassingh118866@gmail.com>
2024-01-29 10:05:02 +0530

    Add function

--- pattern/Question1.java
+++ pattern/Question1.java
@@ -1,4 +1,17 @@
 package DSA_with_JAVA.pattern;
 
 public class Question1 {
+    public static void patt(int n) {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*" );
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+         patt(4);
+    }
 }
+
