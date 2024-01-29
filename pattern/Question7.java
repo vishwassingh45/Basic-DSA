d8c320f9b35e5e4df4b81bec623be39c239e8ddb
Vishwas Kumar <vishwassingh118866@gmail.com>
2024-01-29 11:14:49 +0530

    seventh commit

---
+++ pattern/Question7.java
@@ -0,0 +1,28 @@
+package DSA_with_JAVA.pattern;
+
+public class Question7 {
+    public static void pattern(int n)
+    {
+        for(int row=1;row<=n;row++)
+        {
+            for(int col=1;col<=7;col++)
+            {
+                if(col>=row && col<=8-row)
+                {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+
+            }
+            System.out.println();
+        }
+
+
+    }
+    public static void main(String[] args) {
+           pattern(4);
+    }
+}
