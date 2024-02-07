063f0ca039b21dcdbc96d4d1e48b102f407c2884
Vishwas Kumar <vishwassingh118866@gmail.com>
2024-02-07 19:58:41 +0530

    commit

--- Array/Question12.java
+++ Array/Question12.java
@@ -1,2 +1,37 @@
-package DSA_with_JAVA.Array;public class Question12 {
+package DSA_with_JAVA.Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Question12 {
+    public static int repeat(int []arr)
+    {
+        int count=0;
+        int start=0,end=arr.length-1;
+
+        while(start<=end)
+        {
+
+            if(arr[start]==arr[end] && start<end)
+            {
+               count++;
+
+
+            }
+
+            start++;
+            end--;
+
+
+
+        }
+        return count;
+
+
+    }
+    public static void main(String[] args) {
+          int []arr={1,5,3,4,3,5,6};
+          int x=repeat(arr);
+        System.out.println(x);
+    }
 }
--- Array/README.md
+++ Array/README.md
@@ -69,4 +69,19 @@ Arr[] = {1, 1, 2, 2, 2, 2, 3}
 Output: 4
 Explanation: 2 occurs 4 times in the
 given array.
+
+
+12. Given an array arr[] of size n, find the first repeating element. The element should occur more than once and the index of its first occurrence should be the smallest.
+
+Example 1:
+
+Input:
+n = 7
+arr[] = {1, 5, 3, 4, 3, 5, 6}
+Output: 2
+Explanation: 
+5 is appearing twice and 
+its first appearence is at index 2 
+which is less than 3 whose first 
+occuring index is 3.
 ```
