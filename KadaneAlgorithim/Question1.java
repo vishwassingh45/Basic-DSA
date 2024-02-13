c2adcce0e6a3463e4150e43dbe1ff27c40563b21
Vishwas Kumar <vishwassingh118866@gmail.com>
2024-02-13 08:40:12 +0530

    commit

---
+++ KadaneAlgorithim/Question1.java
@@ -0,0 +1,22 @@
+package DSA_with_JAVA.KadaneAlgorithim;
+
+public class Question1 {
+    public static void prefixSum(int[]arr)
+    {
+        int []x=new int[arr.length];
+
+        x[0]=arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            x[i]=x[i-1]+arr[i];
+        }
+        System.out.print("Prefix sum : ");
+        for (int i : x) {
+            System.out.print(" " + i);
+        }
+    }
+    public static void main(String[] args) {
+        int []arr={6,4,5,-3,2,8};
+        prefixSum(arr);
+    }
+}
---
+++ KadaneAlgorithim/README.md
@@ -0,0 +1,6 @@
+```text
+1. Prefix sum
+
+Example :- arr=[6,4,5,-3,2,8]
+ Output:- [6,10,15,12,14,22]
+```
