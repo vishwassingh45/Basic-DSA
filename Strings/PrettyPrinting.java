880cb2604ce65251466294449fc1ad5c29cf68db
Vishwas Kumar <vishwassingh118866@gmail.com>
2024-02-17 20:05:37 +0530

    commit

---
+++ Strings/PrettyPrinting.java
@@ -0,0 +1,14 @@
+package DSA_with_JAVA.Strings;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a= 453.1234f;
+        //System.out.printf("\nFormatted number is %.2f" , a);
+
+        System.out.printf("%.4f",Math.PI);
+
+        System.out.println();
+
+        System.out.printf("Hello my name is %s ans I am %s ", "Vishwas" ,"Coder");
+    }
+}
