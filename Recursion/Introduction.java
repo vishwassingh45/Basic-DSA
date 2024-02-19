d694925cb797e5cd5aaf02c6cb4dd588bdc99f45
Vishwas Kumar <vishwassingh118866@gmail.com>
2024-02-19 08:47:48 +0530

    commit

--- Recursion/Introduction.java
+++ Recursion/Introduction.java
@@ -1,12 +1,26 @@
 package DSA_with_JAVA.Recursion;
 
+/*
+         RECURSION
+      ----------------
+      A function which calls itself again and  again until a specific condition met.
+
+    */
 public class Introduction {
+    public static void Birthday(int n)
+    {
+        // Base Case
+        if(n==0)
+        {
+            System.out.println("❤️ Happy Birthday ❤️");
+        }
+        else{
+            System.out.println(n + " day left");
+            Birthday(n-1);
+        }
+    }
     public static void main(String[] args) {
-        /*
-            RECURSION
-         ----------------
-         A function which calls itself again and  again until a specific condition met.
-       */
+        Birthday(5);
 
     }
 }
