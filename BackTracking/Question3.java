381ad2c3947a70e0aec1f16a91eef9cc8f9e795b
Vishwas Kumar <vishwassingh118866@gmail.com>
2024-02-26 11:15:21 +0530

    commit

--- BackTracking/Question3.java
+++ BackTracking/Question3.java
@@ -4,27 +4,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Question3 {
-    public static void subset(List<List<Integer>>list, List<Integer>dummy, int []nums, int index)
-    {
-        // Base case
-
-        if(index==nums.length)
-        {
-            list.add(new ArrayList<>(dummy));
-            return;
-        }
-
-        // choice no
-        subset(list,dummy,nums,index+1);
-        // choice Yes
-        dummy.add(nums[index]);
-        subset(list,dummy,nums,index+1);
-    }
     public static void main(String[] args) {
-        int []nums={1,2,3};
-        List<List<Integer>>list=new ArrayList<>();
-        subset(list,new ArrayList<>(),nums,0);
+        /*
+          void Subsquence(vector<int>& arr,int index, int n,vector<vector<int>>&ans,vector<int>dummy)
+      {
+         if(index==n)
+         {
+           ans.push_back(dummy);
+           return;
+         }
 
+         // Choice no
 
+         Subsquence(arr,index+1,n,ans,dummy);
+         //For Yes
+
+         dummy.push_back(arr[index]);
+          Subsquence(arr,index+1,n,ans,dummy);
+      }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>dummy;
+        Subsquence(nums,0,nums.size(),ans,dummy);
+
+        return ans;
+    }
+         */
     }
 }
